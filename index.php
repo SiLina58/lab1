3fc15c0f5d78feb3b37ea320d2219ed5ed2dafa1
Виктория <calipesto@inbox.ru>
2020-12-30 00:10:52 +0300

    первая лаба

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Среднее арифметическое трёх цифр: 8, 1 и 6 равно:";
+				echo (8 + 1 + 6)/3;
+			?>
+		</div>
+	</body>
+</html>
